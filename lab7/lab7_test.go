78a7dc86f73814514773e94fc54acfd8d1773039
Ally Dale <vipally@163.com>
2017-03-01 16:01:13 +0800

    interface inherit test

---
+++ lab7/lab7_test.go
@@ -0,0 +1,31 @@
+package lab7_test
+
+import (
+	"fmt"
+	"testing"
+)
+
+type Iter interface {
+	Do1()
+	Do2()
+}
+type father struct {
+}
+
+func (this *father) Do1() { fmt.Println("father Do1") }
+func (this *father) Do2() { fmt.Println("father Do2") }
+
+type child struct {
+	father
+}
+
+func (this *child) Do1() {
+	this.father.Do1()
+	fmt.Println("child Do1")
+}
+
+func TestInterface(t *testing.T) {
+	var i Iter = new(child)
+	i.Do1()
+	i.Do2()
+}
