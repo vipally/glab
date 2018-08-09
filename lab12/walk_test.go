610183dbc4df1002ea10770bd9c8e5e75c674ef6
Ally Dale <vipally@gmail.com>
2018-08-09 09:14:00 +0800

    update

--- lab12/walk_test.go
+++ lab12/walk_test.go
@@ -8,23 +8,23 @@ import (
 )
 
 func TestWalk(t *testing.T) {
-	var itPet pet.Walker
-	var itFilePath filepath.Walker
+	var petWalker pet.Walker
+	var filepathWalker filepath.Walker
 	println("call by pet.Walker:")
-	itPet = &pet.Dog{}
-	itPet.Walk()
-	itPet = &pet.Cat{}
-	itPet.Walk()
-	itPet = &filepath.FilePath{}
-	itPet.Walk()
+	petWalker = &pet.Dog{}
+	petWalker.Walk()
+	petWalker = &pet.Cat{}
+	petWalker.Walk()
+	petWalker = &filepath.FilePath{}
+	petWalker.Walk()
 
 	println("\ncall by filepath.Walker:")
-	itFilePath = &pet.Dog{}
-	itFilePath.Walk()
-	itFilePath = &pet.Cat{}
-	itFilePath.Walk()
-	itFilePath = &filepath.FilePath{}
-	itFilePath.Walk()
+	filepathWalker = &pet.Dog{}
+	filepathWalker.Walk()
+	filepathWalker = &pet.Cat{}
+	filepathWalker.Walk()
+	filepathWalker = &filepath.FilePath{}
+	filepathWalker.Walk()
 
 	println("\nThe strange is that pet.Warker and filepath.Walker has the same signiture but they are not the same one.")
 	println("But Go treat them as the same one.")
