2e76449f6348e3f370bbb6a2059b64d2638f3e42
Ally Dale <vipally@gmail.com>
2018-11-09 23:57:26 +0800

    time test

--- lab13/lab13.go
+++ lab13/lab13.go
@@ -31,36 +31,41 @@ func main() {
 	sysNow := time.Now()
 	start = sysNow
 	myNow := fmtTime(sysNow)
-	fmt.Printf("sysNow: %-60s %#v\n", sysNow.String(), timeStruct(sysNow))
-	fmt.Printf("myNow : %-60s %#v\n", myNow.String(), timeStruct(myNow))
+	fmt.Printf("sysNow: %-60s %d %#v\n", sysNow.String(), sysNow.Unix(), timeStruct(sysNow))
+	fmt.Printf("myNow : %-60s %d %#v\n", myNow.String(), myNow.Unix(), timeStruct(myNow))
 
 	d := time.Second * 10
 	dest := sysNow.Add(d)
-	fmt.Println("dest:", d.String(), dest)
+	fmt.Printf("dest : %s %-60s %d %#v\n", d.String(), dest.String(), dest.Unix(), timeStruct(dest))
 	time.AfterFunc(d, afterFunc)
 
 	t := time.NewTimer(d)
 	//modify system time here...
-	modifySystime(-3)
+	modify := -3
+	modifySystime(modify)
 	after := <-t.C
-	fmt.Printf("after: %-60s %#v\n", after.String(), timeStruct(after))
+	fmt.Printf("after: %-60s %d %#v\n", after.String(), after.Unix(), timeStruct(after))
 	fmt.Printf("diffSysNow: %s\n", after.Sub(sysNow)) //not rely to sysytem time
 	fmt.Printf("diffMyNow: %s\n", after.Sub(myNow))   //rely to system time
 
-	//sysNow: 2018-11-08 11:30:06.56078 +0800 CST m=+0.006000301           main.timeStruct{wall:0xbef1094fa16cd2e0, ext:6000301, loc:(*time.Location)(0x579300)}
-	//myNow : 2018-11-08 11:30:06 +0800 CST                                main.timeStruct{wall:0x0, ext:63677244606, loc:(*time.Location)(0x579300)}
-	//dest: 10s 2018-11-08 11:30:16.56078 +0800 CST m=+10.006000301
-	//after: 2018-11-08 11:30:09.608378 +0800 CST m=+10.076576301         main.timeStruct{wall:0xbef1095064431c90, ext:10076576301, loc:(*time.Location)(0x579300)}
-	//logic diff 10.070576s
-	//after: 2018-11-08 11:30:09.608378 +0800 CST m=+10.076576301         main.timeStruct{wall:0xbef1095064431c90, ext:10076576301, loc:(*time.Location)(0x579300)}
-	//real diff 3.608378s
-	//diffSysNow: 10.070576s
-	//diffMyNow: 3.608378s
+	modifySystime(-modify)
+
+	//sysNow: 2018-11-09 23:52:48.7326954 +0800 CST m=+0.015600101         1541778768 main.timeStruct{wall:0xbef189342bac0b68, ext:15600101, loc:(*time.Location)(0x579c20)}
+	//myNow : 2018-11-09 23:52:48 +0800 CST                                1541778768 main.timeStruct{wall:0x0, ext:63677375568, loc:(*time.Location)(0x579c20)}
+	//dest:10s 2018-11-09 23:52:58.7326954 +0800 CST m=+10.015600101       1541778778 main.timeStruct{wall:0xbef18936abac0b68, ext:10015600101, loc:(*time.Location)(0x579c20)}
+	//modifySystime -3
+	//after: 2018-11-09 23:52:54.9771123 +0800 CST m=+10.047314601         1541778774 main.timeStruct{wall:0xbef18935ba3d8cec, ext:10047314601, loc:(*time.Location)(0x579c20)}
+	//diffSysNow: 10.0317145s
+	//diffMyNow: 6.9771123s
+	//modifySystime 3
+	//afterf: 2018-11-09 23:52:54.9771123 +0800 CST m=+10.047314601        1541778774 main.timeStruct{wall:0xbef18935ba3d8cec, ext:10047314601, loc:(*time.Location)(0x579c20)}
+	//logic diff 10.0317145s
+	//real diff 6.9771123s
 }
 
 func afterFunc() {
 	t := time.Now()
-	fmt.Printf("afterf: %-60s %#v\n", t, timeStruct(t)) //10second,but when system time change during sleep, real time is not correct
+	fmt.Printf("afterf: %-60s %d %#v\n", t, t.Unix(), timeStruct(t)) //10second,but when system time change during sleep, real time is not correct
 	fmt.Println("logic diff", t.Sub(start).String())
 	myStart := fmtTime(start)
 	fmt.Println("real diff", t.Sub(myStart).String())
