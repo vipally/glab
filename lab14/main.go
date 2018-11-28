b079c604ae9d4057f8dc7edd978f36cee0fcc90c
Ally Dale <vipally@gmail.com>
2018-11-28 10:04:35 +0800

    update

--- lab14/main.go
+++ lab14/main.go
@@ -1,6 +1,11 @@
 //针对1写N读的常见多线程应用案例
 //分别应用mutext加锁和channel的方法 做多线程同步
 //测试运行效率
+//从测试结果可以看出 加锁的方式执行效率较低
+//这里设置了一个典型的测试点 每一秒会触发一次较耗时的操作costLongTimeAndGosched
+//加锁方式下，这个操作相当于是串行执行，因此效率较低。
+//而channel模式，共享的frame是通过channel通知到每个Reader线程，因此不存在资源竞争的情况，天然可并行
+//这种模型也更符合go语言推荐的多线程模型。
 
 package main
 
@@ -14,15 +19,16 @@ import (
 
 const (
 	mps           = 20
-	finishSecs    = 600
-	readerCount   = 5000
+	finishSecs    = 300
+	readerCount   = 2000
 	busyCount     = 50
+	chLen         = 5
 	schduleInLock = true
 )
 
 func init() {
 	for i := len(chs) - 1; i >= 0; i-- {
-		chs[i] = make(chan int, 2)
+		chs[i] = make(chan int, chLen)
 	}
 	runtime.GOMAXPROCS(runtime.NumCPU())
 }
@@ -42,7 +48,7 @@ func main() {
 	start := time.Now()
 	costLongTimeAndGosched()
 	now := time.Now()
-	fmt.Println(start, now, "costLongTimeAndGosched fnish,cost ", now.Sub(start))
+	fmt.Println(start, now, "costLongTimeAndGosched finish,cost ", now.Sub(start))
 	Mutex1WnR()
 	Channel1WnR()
 }
@@ -58,7 +64,7 @@ func Mutex1WnR() {
 	//return
 	frame = 0
 	start := time.Now()
-	fmt.Printf("TestMutex1WnR start time=%s, finishSecs=%d readerCount=%d,busyCount=%d\n", start.Format("2006-01-02 15:04:05"), finishSecs, readerCount, busyCount)
+	fmt.Printf("Mutex1WnR start time=%s, finishSecs=%d readerCount=%d,busyCount=%d\n", start.Format("2006-01-02 15:04:05"), finishSecs, readerCount, busyCount)
 	wg.Add(1 + readerCount)
 	go w()
 	for i := 1; i <= readerCount; i++ {
@@ -69,7 +75,7 @@ func Mutex1WnR() {
 	}
 	wg.Wait()
 	now := time.Now()
-	fmt.Println(now, "TestMutex1WnR finish,cost ", finishSecs, "->", now.Sub(start))
+	fmt.Println(now, "Mutex1WnR finish,cost ", finishSecs, "->", now.Sub(start))
 }
 
 func w() {
@@ -140,7 +146,7 @@ func busy(id int) {
 func Channel1WnR() {
 	frame = 0
 	start := time.Now()
-	fmt.Printf("TestMutex1WnR start time=%s, finishSecs=%d readerCount=%d,busyCount=%d\n", start.Format("2006-01-02 15:04:05"), finishSecs, readerCount, busyCount)
+	fmt.Printf("Mutex1WnR start time=%s, finishSecs=%d readerCount=%d,busyCount=%d\n", start.Format("2006-01-02 15:04:05"), finishSecs, readerCount, busyCount)
 	wg.Add(1 + readerCount)
 	for i := 0; i < readerCount; i++ {
 		go chR(i)
@@ -151,7 +157,7 @@ func Channel1WnR() {
 	}
 	wg.Wait()
 	now := time.Now()
-	fmt.Println(now, "TestChannel1WnR finish,cost ", finishSecs, "->", now.Sub(start))
+	fmt.Println(now, "Channel1WnR finish,cost ", finishSecs, "->", now.Sub(start))
 }
 
 func chTellReaders(f int) {
