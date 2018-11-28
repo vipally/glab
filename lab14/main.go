9ddd3e2a7b0427e3a1446b6f808fb3202c91eca8
Ally Dale <vipally@gmail.com>
2018-11-28 09:29:20 +0800

    update

--- lab14/main.go
+++ lab14/main.go
@@ -1,3 +1,7 @@
+//针对1写N读的常见多线程应用案例
+//分别应用mutext加锁和channel的方法 做多线程同步
+//测试运行效率
+
 package main
 
 import (
@@ -10,24 +14,35 @@ import (
 
 const (
 	mps           = 20
-	finishSecs    = 300
-	readerCount   = 2000
+	finishSecs    = 600
+	readerCount   = 5000
 	busyCount     = 50
 	schduleInLock = true
 )
 
 func init() {
 	for i := len(chs) - 1; i >= 0; i-- {
-		chs[i] = make(chan int, 1)
+		chs[i] = make(chan int, 2)
 	}
 	runtime.GOMAXPROCS(runtime.NumCPU())
 }
 
+//chLen=2
+// TestMutex1WnR start time=2018-11-28 09:02:36, finishSecs=300 readerCount=2000,busyCount=50
+// 2018-11-28 09:07:59.5806564 +0800 CST m=+323.000474601 TestMutex1WnR fnish,cost 300 -> 5m22.9914741s
+// TestMutex1WnR start time=2018-11-28 09:07:59, finishSecs=300 readerCount=2000,busyCount=50
+// 2018-11-28 09:13:00.9508938 +0800 CST m=+624.370712001 TestChannel1WnR fnish,cost  300 -> 5m1.2482304s
+
+//chLen=1
 // TestMutex1WnR start time=2018-11-27 21:09:34, finishSecs=300 readerCount=2000,busyCount=50
 // 2018-11-27 21:15:05.8786637 +0800 CST m=+331.694971901 TestMutex1WnR fnish,cost  300 -> 5m31.6899716s
 // TestMutex1WnR start time=2018-11-27 21:15:05, finishSecs=300 readerCount=2000,busyCount=50
 // 2018-11-27 21:20:44.4450285 +0800 CST m=+670.261336701 TestChannel1WnR fnish,cost  300 -> 5m38.5663648s
 func main() {
+	start := time.Now()
+	costLongTimeAndGosched()
+	now := time.Now()
+	fmt.Println(start, now, "costLongTimeAndGosched fnish,cost ", now.Sub(start))
 	Mutex1WnR()
 	Channel1WnR()
 }
@@ -99,8 +114,9 @@ func r(id int) {
 
 func costLongTimeAndGosched() {
 	b := big.NewInt(100)
-	for i := 1; i <= 100; i++ {
-		if i == 50 {
+	const count = 2000
+	for i := 1; i <= count; i++ {
+		if i == count/2 {
 			runtime.Gosched()
 		}
 		b.MulRange(int64(i), int64(i+1))
