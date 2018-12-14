1e3a98f3c133e54e8c4c208af87ba01682f33873
Ally Dale <vipally@gmail.com>
2018-12-14 15:15:05 +0800

    dice test

--- lab15/dice_test.go
+++ lab15/dice_test.go
@@ -67,3 +67,93 @@ func showResult(name string, point int, count int) {
 	rate := count * 10000 / randCount
 	fmt.Printf("%-8s p=%d count=%d/%d rate=%d.%d%%\n", name, point, count, randCount, rate/100, rate%100)
 }
+
+// 押注方案 100(大小) x 2 + 0(豹子)*6
+// 01 times=058 WinCoin=-200 WinScore=11800 ScorePerCoin=59.00
+// 02 times=149 WinCoin=-200 WinScore=30000 ScorePerCoin=150.00
+// 03 times=018 WinCoin=-200 WinScore=03800 ScorePerCoin=19.00
+// 04 times=107 WinCoin=-200 WinScore=21600 ScorePerCoin=108.00
+// 05 times=076 WinCoin=-200 WinScore=15400 ScorePerCoin=77.00
+// 06 times=042 WinCoin=-200 WinScore=08600 ScorePerCoin=43.00
+// 07 times=026 WinCoin=-200 WinScore=05400 ScorePerCoin=27.00
+// 08 times=001 WinCoin=-200 WinScore=00400 ScorePerCoin=2.00
+// 09 times=012 WinCoin=-200 WinScore=02600 ScorePerCoin=13.00
+// 10 times=066 WinCoin=-200 WinScore=13400 ScorePerCoin=67.00
+// totalBet=565 totalWinCoin=-2000 totalWinScore=113000, totalScorePerCoin=56.50
+// 押注方案 97(大小) x 2 + 1(豹子)*6
+// 01 times=007 WinCoin=-091 WinScore=01600 ScorePerCoin=17.58
+// 02 times=062 WinCoin=-421 WinScore=12600 ScorePerCoin=29.93
+// 03 times=104 WinCoin=-673 WinScore=21000 ScorePerCoin=31.20
+// 04 times=069 WinCoin=-463 WinScore=14000 ScorePerCoin=30.24
+// 05 times=075 WinCoin=-499 WinScore=15200 ScorePerCoin=30.46
+// 06 times=050 WinCoin=-349 WinScore=10200 ScorePerCoin=29.23
+// 07 times=050 WinCoin=-349 WinScore=10200 ScorePerCoin=29.23
+// 08 times=000 WinCoin=-049 WinScore=00200 ScorePerCoin=4.08
+// 09 times=004 WinCoin=-073 WinScore=01000 ScorePerCoin=13.70
+// 10 times=065 WinCoin=-439 WinScore=13200 ScorePerCoin=30.07
+// totalBet=496 totalWinCoin=-3406 totalWinScore=99200, totalScorePerCoin=29.13
+// 押注方案 94(大小) x 2 + 2(豹子)*6
+// 01 times=008 WinCoin=0006 WinScore=01800 ScorePerCoin=-
+// 02 times=021 WinCoin=-150 WinScore=04400 ScorePerCoin=29.33
+// 03 times=014 WinCoin=-066 WinScore=03000 ScorePerCoin=45.45
+// 04 times=039 WinCoin=-366 WinScore=08000 ScorePerCoin=21.86
+// 05 times=015 WinCoin=-078 WinScore=03200 ScorePerCoin=41.03
+// 06 times=004 WinCoin=0054 WinScore=01000 ScorePerCoin=-
+// 07 times=023 WinCoin=-174 WinScore=04800 ScorePerCoin=27.59
+// 08 times=005 WinCoin=0042 WinScore=01200 ScorePerCoin=-
+// 09 times=015 WinCoin=-078 WinScore=03200 ScorePerCoin=41.03
+// 10 times=015 WinCoin=-078 WinScore=03200 ScorePerCoin=41.03
+// totalBet=169 totalWinCoin=-888 totalWinScore=33800, totalScorePerCoin=38.06
+// 押注方案 91(大小) x 2 + 3(豹子)*6
+// 01 times=062 WinCoin=-863 WinScore=12600 ScorePerCoin=14.60
+// 02 times=018 WinCoin=-071 WinScore=03800 ScorePerCoin=53.52
+// 03 times=071 WinCoin=-1025 WinScore=14400 ScorePerCoin=14.05
+// 04 times=027 WinCoin=-233 WinScore=05600 ScorePerCoin=24.03
+// 05 times=017 WinCoin=-053 WinScore=03600 ScorePerCoin=67.92
+// 06 times=029 WinCoin=-269 WinScore=06000 ScorePerCoin=22.30
+// 07 times=004 WinCoin=0181 WinScore=01000 ScorePerCoin=-
+// 08 times=007 WinCoin=0127 WinScore=01600 ScorePerCoin=-
+// 09 times=016 WinCoin=-035 WinScore=03400 ScorePerCoin=97.14
+// 10 times=003 WinCoin=0199 WinScore=00800 ScorePerCoin=-
+// totalBet=264 totalWinCoin=-2042 totalWinScore=52800, totalScorePerCoin=25.86
+func TestScore(t *testing.T) {
+	const (
+		baozi     = 0
+		littleBig = 100 - baozi*3
+	)
+
+	fmt.Printf("押注方案 %d(大小) x 2 + %d(豹子)*6\n", littleBig, baozi)
+
+	totalBet := 0
+	totalWinCoin := 0
+	totalWinScore := 0
+	for i := 0; i < 10; i++ {
+		n := getWinTimes()
+		WinCoin := 150*baozi - n*6*baozi - littleBig*2 - baozi*5
+		WinScore := 200 * (n + 1)
+		totalBet += (n + 1)
+		totalWinCoin += WinCoin
+		totalWinScore += WinScore
+		ScorePerCoin := "-"
+		if WinCoin < 0 {
+			ScorePerCoin = fmt.Sprintf("%.2f", float64(WinScore)/float64(-WinCoin))
+		}
+		fmt.Printf("%02d times=%03d WinCoin=%04d WinScore=%05d ScorePerCoin=%s\n", i+1, n, WinCoin, WinScore, ScorePerCoin)
+	}
+	totalScorePerCoin := "-"
+	if totalWinCoin < 0 {
+		totalScorePerCoin = fmt.Sprintf("%.2f", float64(totalWinScore)/float64(-totalWinCoin))
+	}
+	fmt.Printf("totalBet=%d totalWinCoin=%d totalWinScore=%d, totalScorePerCoin=%s\n", totalBet, totalWinCoin, totalWinScore, totalScorePerCoin)
+}
+
+func getWinTimes() int {
+	count := 0
+	for {
+		_, _, bz := randDice()
+		if bz {
+			return count
+		}
+		count++
+	}
+}
