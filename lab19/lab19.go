3582ecdffaa913e39a6b37f7d5becff981b23b2d
Ally Dale <vipally@gmail.com>
2019-07-29 09:35:29 +0800

    test gotoutine

--- lab19/lab19.go
+++ lab19/lab19.go
@@ -1,48 +1,52 @@
 package main
 
 import (
-	//"runtime"
+	"runtime"
 	"sync"
 	"time"
 )
 
-const workerNum = 1000000000
+const workerNum = 100000
 const outerLoop = 1000
 const innerLoop = 10000
 
 var wg sync.WaitGroup
 
 func work(id int) {
+	check := workerNum
 	sum := 0
 	start := time.Now()
 	last := time.Now()
 	for i := 1; i <= outerLoop; i++ {
 		for j := 1; j <= innerLoop; j++ {
 			sum += i * j
-			if id != 1 {
-				if j%100 == 0 {
-					time.Sleep(time.Microsecond)
-				}
-			}
+			// if id != check {
+			// 	if j%1000 == 0 {
+			// 		//time.Sleep(time.Microsecond)
+			// 		runtime.Gosched()
+			// 	}
+			// }
 		}
-		if id == 1 {
+		if id == check {
 			now := time.Now()
 			println(i, now.String(), now.Sub(last).String())
 			last = now
 		}
-		//runtime.Gosched()
+		runtime.Gosched()
 	}
-	if id == 1 {
+	if id == check {
 		println("finish, cost =", time.Now().Sub(start).String())
 	}
 	wg.Done()
-	println("all finish, cost =", time.Now().Sub(start).String())
+
 }
 
 func main() {
+	start := time.Now()
 	for i := 1; i <= workerNum; i++ {
 		wg.Add(1)
 		go work(i)
 	}
 	wg.Wait()
+	println("all finish, cost =", time.Now().Sub(start).String())
 }
