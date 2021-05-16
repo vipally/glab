063b065c14145df65e429a93f143de81ccf20fc4
Ally Dale <vipally@gmail.com>
2021-05-16 21:57:58 +0800

    update

--- lab10/version4.go
+++ lab10/version4.go
@@ -9,33 +9,13 @@ type Version4 struct {
 	maxDepth  int
 	fnCallCnt int
 	known     []float64
-	stk       Stack
-}
-
-type Stack []int
-
-func (s *Stack) Push(n int) {
-	*s = append(*s, n)
-}
-func (s *Stack) Pop() int {
-	if sz := len(*s); sz > 0 {
-		n := (*s)[sz-1]
-		*s = (*s)[:sz-1]
-		return n
-	}
-	return -1
-}
-func (s Stack) Size() int {
-	return len(s)
-}
-func (s Stack) Empty() bool {
-	return s.Size() == 0
 }
 
 func (v *Version4) R(n, depth int) (percent float64) {
-	if depth > 3 { // limit depth
+	if depth > 3 { // assert recursive depth
 		panic(depth)
 	}
+
 	if depth == 1 {
 		v.fnCallCnt = 0
 		v.maxDepth = 1
@@ -44,22 +24,17 @@ func (v *Version4) R(n, depth int) (percent float64) {
 	if depth > v.maxDepth {
 		v.maxDepth = depth
 	}
-	switch {
-	case n <= len(v.known): //result is known, use the result
+
+	if n <= len(v.known) { //result is known, use the result directlly
 		return v.known[n-1]
-	case depth == 1 && n > 2:
-		for i := len(v.known) + 1; i < n; i++ { // reverse the call direction like stack
-			v.R(i, 2)
-		}
-	default:
-		// do nothing
 	}
 
 	v.fnCallCnt++
 
-	//println(n, depth, len(v.known), v.stk.Size())
+	//println(n, depth, len(v.known))
 	sum := float64(0)
-	for x := int(1); x <= n; x++ {
+	//for x := int(1); x <= n; x++ {
+	for x := n; x >= 1; x-- { // reverse order to let R(1~n) evaluate order
 		s := v.s(n, x, depth)
 		sum += s
 	}
@@ -81,8 +56,8 @@ func (v *Version4) s(n, x, depth int) (percent float64) {
 }
 
 func main4() {
-	NN := 100000
-	for N := NN; N <= NN; N++ {
+	M := 50
+	for N := M; N <= M; N++ {
 		v := &Version4{}
 		start := time.Now()
 		rate := v.R(N, 1)
@@ -90,5 +65,7 @@ func main4() {
 		fmt.Printf("v4 N=%-2d R=%-5.1f T=%-10s RD=%-2d Fc=%d\n", N, rate, dur, v.maxDepth, v.fnCallCnt)
 	}
 	// output:
-
+	// v4 N=50     R=50.0  T=0s         RD=3  Fc=50
+	// v4 N=50000  R=50.0  T=10.136s    RD=3  Fc=50000
+	// v4 N=100000 R=50.0  T=40.075s    RD=3  Fc=100000
 }
